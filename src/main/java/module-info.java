225a4a9524d79d500e595d38bf16bc8d42af5e6b
sergi <sergio.negro@educa.madrid.org>
2026-04-05 18:37:48 +0200

    Commit 1 - Estructura inicial del proyecto

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,12 @@
 module org.example.tfgjavafx {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.net.http;
+    requires com.fasterxml.jackson.databind;
 
     opens org.example.tfgjavafx to javafx.fxml;
+    opens org.example.tfgjavafx.controller to javafx.fxml;
+    opens org.example.tfgjavafx.model to javafx.base, javafx.controls, javafx.fxml;
+
     exports org.example.tfgjavafx;
 }
